055bee897252f12138056b8037e29fed479a131e
areumJeong <add5833@naver.com>
2024-01-08 15:30:22 +0900

    JS - 표준내장객체

---
+++ 03.JavaScript/ch07.표준내장객체/04.String연습.js
@@ -0,0 +1,25 @@
+// 연습문제
+// 1. 1에서 1000사이에 0은 몇번, 1은 몇번, ..., 9는 몇번 사용되었는가?
+//      1234567890101112131415...9991000
+let numStr = '';
+for (let i = 1; i <= 1000; i++) {
+    numStr += i;
+}
+
+// 정규표현식 활용
+// 1이 몇번 사용되었나?
+console.log(numStr.replace(/[^1]/g, '').length); 
+
+for (let i = 0; i <= 9; i++){
+    let pattern = new RegExp('[^' + i + ']', 'g'); // 반복 시 RegExp 객체 생성해서 반복 
+    count = numStr.replace(pattern, '').length;
+    console.log(`${i}: ${count}`);
+}
+
+// count 배열을 만들어 일일이 체크
+let countArr = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+for (let i = 0; i < numStr.length; i++) {
+    let num = Number(numStr[i]);
+    countArr[num]++;
+}
+console.log(countArr);
